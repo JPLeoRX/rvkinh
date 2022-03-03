7b4e4cf55f2838872e918f413df06a3f9a3c5057
leo <leo.ertuna@gmail.com>
2022-03-03 18:35:52 +0300

    Startup and shutdown of http flood job

--- main.py
+++ main.py
@@ -32,19 +32,23 @@ app.include_router(router_ping)
 app.include_router(router_portscan)
 
 # Initialize jobs
-from jobs.jobs_registry import JOB_ATTACK_SYN_FLOOD, JOB_ATTACK_PING_FLOOD
-thread1 = threading.Thread(target=JOB_ATTACK_SYN_FLOOD.job_loop)
+from jobs.jobs_registry import JOB_ATTACK_HTTP_FLOOD, JOB_ATTACK_PING_FLOOD, JOB_ATTACK_SYN_FLOOD
+thread1 = threading.Thread(target=JOB_ATTACK_HTTP_FLOOD.job_loop)
 thread1.start()
 thread2 = threading.Thread(target=JOB_ATTACK_PING_FLOOD.job_loop)
 thread2.start()
+thread3 = threading.Thread(target=JOB_ATTACK_SYN_FLOOD.job_loop)
+thread3.start()
 
 
 # Shutdown handler
 @app.on_event('shutdown')
 def shutdown_event():
     print('shutdown_event(): Started')
-    JOB_ATTACK_SYN_FLOOD.kill()
+    JOB_ATTACK_HTTP_FLOOD.kill()
     thread1.join()
     JOB_ATTACK_PING_FLOOD.kill()
     thread2.join()
+    JOB_ATTACK_SYN_FLOOD.kill()
+    thread3.join()
     print('shutdown_event(): Finished')
