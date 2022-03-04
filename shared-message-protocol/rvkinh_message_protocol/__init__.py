335c97323a250199c2b653f81b58f1c7dbd796f6
leo <leo.ertuna@gmail.com>
2022-03-04 13:49:55 +0300

    Fixed init script

--- shared-message-protocol/rvkinh_message_protocol/__init__.py
+++ shared-message-protocol/rvkinh_message_protocol/__init__.py
@@ -1,3 +1,29 @@
-__all__ = [
+from .job.job_attack_http_flood_start_input import JobAttackHttpFloodStartInput
+from .job.job_attack_http_flood_status_output import JobAttackHttpFloodStatusOutput
+from .job.job_attack_ping_flood_start_input import JobAttackPingFloodStartInput
+from .job.job_attack_ping_flood_status_output import JobAttackPingFloodStatusOutput
+from .job.job_attack_syn_flood_start_input import JobAttackSynFloodStartInput
+from .job.job_attack_syn_flood_status_output import JobAttackSynFloodStatusOutput
 
+from .portscan.portscan_check_all_input import PortscanCheckAllInput
+from .portscan.portscan_check_all_output import PortscanCheckAllOutput
+from .portscan.portscan_check_multiple_input import PortscanCheckMultipleInput
+from .portscan.portscan_check_multiple_output import PortscanCheckMultipleOutput
+from .portscan.portscan_check_single_input import PortscanCheckSingleInput
+from .portscan.portscan_check_single_output import PortscanCheckSingleOutput
+
+__all__ = [
+    JobAttackHttpFloodStartInput,
+    JobAttackHttpFloodStatusOutput,
+    JobAttackPingFloodStartInput,
+    JobAttackPingFloodStatusOutput,
+    JobAttackSynFloodStartInput,
+    JobAttackSynFloodStatusOutput,
+
+    PortscanCheckAllInput,
+    PortscanCheckAllOutput,
+    PortscanCheckMultipleInput,
+    PortscanCheckMultipleOutput,
+    PortscanCheckSingleInput,
+    PortscanCheckSingleOutput
 ]
