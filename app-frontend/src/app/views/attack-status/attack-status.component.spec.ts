d20f011b3ff34a006a347acc71839cde5ff12b4c
leo <leo.ertuna@gmail.com>
2022-04-16 17:28:41 +0300

    Attack status panel

---
+++ app-frontend/src/app/views/attack-status/attack-status.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { AttackStatusComponent } from './attack-status.component';
+
+describe('AttackStatusComponent', () => {
+  let component: AttackStatusComponent;
+  let fixture: ComponentFixture<AttackStatusComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ AttackStatusComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(AttackStatusComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ app-frontend/src/app/views/attack-status/attack-status.component.ts
@@ -0,0 +1,83 @@
+import { Component, OnInit } from '@angular/core';
+import {AttackOrchestration} from "../../message-protocol/attack-orchestration.model";
+import {ClientService} from "../../services/client.service";
+import {Worker} from "../../message-protocol/worker.model";
+
+@Component({
+  selector: 'app-attack-status',
+  templateUrl: './attack-status.component.html',
+  styleUrls: ['./attack-status.component.css']
+})
+export class AttackStatusComponent implements OnInit {
+  loadingWorkers: boolean = false;
+  hasResultWorkers: boolean = false;
+  hasErrorWorkers: boolean = false;
+  listOfWorkers: Worker[] = [];
+  listOfClusters: string[] = [];
+
+  loadingStatus: boolean = false;
+  hasResultStatus: boolean = false;
+  hasErrorStatus: boolean = false;
+  attackOrchestrationStatus: AttackOrchestration = null!;
+
+
+  constructor(private _clientService: ClientService) { }
+
+  ngOnInit(): void {
+    this.loadWorkers();
+    this.loadStatus()
+  }
+
+  loadWorkers() {
+    this.hasResultWorkers = false;
+    this.hasErrorWorkers = false;
+    this.loadingWorkers = true;
+
+    this._clientService.workersAlive().subscribe({
+      next: (output: Worker[]) => {
+        this.listOfWorkers = output;
+        this.listOfClusters = Array.from(new Set(this.listOfWorkers.map(e => e.cluster_id)));
+        this.loadingWorkers = false;
+        this.hasErrorWorkers = false;
+        this.hasResultWorkers = true;
+      },
+      error: (err) => {
+        this.loadingWorkers = false;
+        this.hasResultWorkers = false;
+        this.hasErrorWorkers = true;
+      }
+    });
+  }
+
+  loadStatus(): void {
+    this.hasResultStatus = false;
+    this.hasErrorStatus = false;
+    this.loadingStatus = true;
+
+    this._clientService.attackOrchestrationStatus().subscribe({
+      next: (output: AttackOrchestration) => {
+        this.attackOrchestrationStatus = output;
+        this.loadingStatus = false;
+        this.hasErrorStatus = false;
+        this.hasResultStatus = true;
+      },
+      error: (err) => {
+        this.loadingStatus = false;
+        this.hasResultStatus = false;
+        this.hasErrorStatus = true;
+      }
+    });
+  }
+
+  isStopped(clusterId: string): boolean {
+    if (this.attackOrchestrationStatus.goal_akio_by_cluster_id.hasOwnProperty(clusterId) && this.attackOrchestrationStatus.goal_haru_by_cluster_id.hasOwnProperty(clusterId)) {
+      let goal_akio = this.attackOrchestrationStatus.goal_akio_by_cluster_id[clusterId];
+      let goal_haru = this.attackOrchestrationStatus.goal_haru_by_cluster_id[clusterId];
+      let is_haru_disabled = goal_haru.http_flood_target_urls.length == 0;
+      let is_akio_disabled = goal_akio.ping_flood_target_ip_address.length == 0 && goal_akio.syn_flood_target_ip_address.length == 0;
+      return is_haru_disabled && is_akio_disabled;
+    } else {
+      return true;
+    }
+  }
+}
