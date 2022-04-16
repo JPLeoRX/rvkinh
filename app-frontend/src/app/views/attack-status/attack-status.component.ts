4aa6171747d0b4d562f96e0fb676197ca3378160
leo <leo.ertuna@gmail.com>
2022-04-17 01:55:53 +0300

    Reload attack status

--- app-frontend/src/app/views/attack-status/attack-status.component.ts
+++ app-frontend/src/app/views/attack-status/attack-status.component.ts
@@ -20,15 +20,20 @@ export class AttackStatusComponent implements OnInit {
   hasErrorStatus: boolean = false;
   attackOrchestrationStatus: AttackOrchestration = null!;
 
-
   constructor(private _clientService: ClientService) { }
 
   ngOnInit(): void {
     this.loadWorkers();
-    this.loadStatus()
+    this.loadStatus();
+
+    setInterval(() => {
+      this.reloadWorkers();
+      this.reloadStatus();
+    }, 3000)
   }
 
   loadWorkers() {
+    console.log('attack-status.component.loadWorkers(): Started')
     this.hasResultWorkers = false;
     this.hasErrorWorkers = false;
     this.loadingWorkers = true;
@@ -50,9 +55,47 @@ export class AttackStatusComponent implements OnInit {
   }
 
   loadStatus(): void {
+    console.log('attack-status.component.loadStatus(): Started')
     this.hasResultStatus = false;
     this.hasErrorStatus = false;
     this.loadingStatus = true;
+
+    this._clientService.attackOrchestrationStatus().subscribe({
+      next: (output: AttackOrchestration) => {
+        this.attackOrchestrationStatus = output;
+        this.loadingStatus = false;
+        this.hasErrorStatus = false;
+        this.hasResultStatus = true;
+      },
+      error: (err) => {
+        this.loadingStatus = false;
+        this.hasResultStatus = false;
+        this.hasErrorStatus = true;
+      }
+    });
+  }
+
+  reloadWorkers() {
+    console.log('attack-status.component.reloadWorkers(): Started')
+
+    this._clientService.workersAlive().subscribe({
+      next: (output: Worker[]) => {
+        this.listOfWorkers = output;
+        this.listOfClusters = Array.from(new Set(this.listOfWorkers.map(e => e.cluster_id)));
+        this.loadingWorkers = false;
+        this.hasErrorWorkers = false;
+        this.hasResultWorkers = true;
+      },
+      error: (err) => {
+        this.loadingWorkers = false;
+        this.hasResultWorkers = false;
+        this.hasErrorWorkers = true;
+      }
+    });
+  }
+
+  reloadStatus(): void {
+    console.log('attack-status.component.reloadStatus(): Started')
 
     this._clientService.attackOrchestrationStatus().subscribe({
       next: (output: AttackOrchestration) => {
