45742d9e14840cc8b5bdbbd3c2c08073e02a2d93
leo <leo.ertuna@gmail.com>
2022-06-26 17:10:07 +0300

    Explained script

--- app/main.sh
+++ app/main.sh
@@ -1,9 +1,13 @@
 #!/bin/sh
+
+# Check the variables exist
 echo $CONTROLLER_URL
 echo $CONTROL_API_KEY
 
+# Check initial config
 cat /app/src/environments/environment.prod.ts
 
+# Replace config
 cat >/app/src/environments/environment.prod.ts <<EOL
 export const environment = {
   production: true,
@@ -12,8 +16,8 @@ export const environment = {
 };
 EOL
 
+# Check resuling config
 cat /app/src/environments/environment.prod.ts
 
-
-
+# Run the app
 ng serve --host 0.0.0.0 --configuration production --live-reload false --disable-host-check true
