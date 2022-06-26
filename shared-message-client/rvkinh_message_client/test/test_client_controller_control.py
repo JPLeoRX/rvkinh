539b269268b30ea0fb83630b2ed59c78973add4e
leo <leo.ertuna@gmail.com>
2022-06-26 10:45:28 +0300

    Updated dockerignore & utils tests

--- shared-message-client/rvkinh_message_client/test/test_client_controller_control.py
+++ shared-message-client/rvkinh_message_client/test/test_client_controller_control.py
@@ -6,6 +6,6 @@ base_url = 'http://142.93.38.82:8888'
 api_key = 'c2uXRF7RLR'
 print('ping():', c.ping(base_url))
 print('system_workers_alive():', c.system_workers_alive(base_url, api_key))
-print('attack_orchestration_status():', c.attack_orchestration_start(base_url, api_key, AttackOrchestration({}, {})))
+#print('attack_orchestration_status():', c.attack_orchestration_start(base_url, api_key, AttackOrchestration({}, {})))
 print('attack_orchestration_status():', c.attack_orchestration_status(base_url, api_key))
-print('attack_orchestration_stop():', c.attack_orchestration_stop(base_url, api_key))
+#print('attack_orchestration_stop():', c.attack_orchestration_stop(base_url, api_key))
